85b865f7c24997f66d227f479e34f7c593d093e5
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2025-12-29 19:16:46 +0000

    Add util

---
+++ AssetHelper/Internal/CollectionUtil.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Silksong.AssetHelper.Internal;
+
+internal static class CollectionUtil
+{
+    /// <summary>
+    /// Return true if self == maybePrefix, or self is of the form {maybePrefix}/...
+    /// </summary>
+    public static bool HasPrefix(this string self, string? maybePrefix)
+    {
+        if (maybePrefix is null)
+        {
+            return false;
+        }
+
+        if (!self.StartsWith(maybePrefix))
+        {
+            return false;
+        }
+
+        if (self == maybePrefix)
+        {
+            return true;
+        }
+
+        if (self[maybePrefix.Length] == '/')
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Given a collection of strings representing game object paths, return a list
+    /// of strings such that none of them has an ancestor in the collection.
+    /// </summary>
+    public static List<string> GetHighestNodes(this ICollection<string> objPaths)
+    {
+        List<string> nodes = [];
+
+        string? last = null;
+
+        foreach (string path in objPaths.OrderBy(x => x))
+        {
+            if (path.HasPrefix(last))
+            {
+                continue;
+            }
+
+            last = path;
+            nodes.Add(path);
+        }
+
+        return nodes;
+    }
+}
--- docs/index.md
+++ docs/index.md
@@ -7,6 +7,9 @@ TODO - usage/installation instructions.
 Docs TODOs (not on this page):
 - Guide on how to find assets you might want to load
 - Production cookbook/examples (various ways to use AssetHelper to load assets in prod)
+  - Mention that it's good to list the exact game objects you need, not their common ancestor.
+  For example, if you need root/child1 and root/child2, let AssetHelper decide whether to
+  repack the root or to repack both child1 and child2 without touching the root.
 - Common gotchas
   - Never modify a prefab before instantiating it
   - Stuff gets reloaded on quit to menu
@@ -23,3 +26,6 @@ Docs TODOs (not on this page):
   - Make sure loading works in at least one scene where the object is likely to spawn, assuming that
     there are a limited set of scenes where spawning will happen
 	- Obvious example would be a duo boss fight where boss A is added to boss B's scene
+  - Make sure to test loading and then changing scene
+  - Make sure to test loading and then returning to menu
+  - Make sure to test loading, returning to menu and then loading again
