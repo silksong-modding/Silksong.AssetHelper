5c4966107947688d5fea026e325f172157a80dc5
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2026-01-02 10:52:12 +0000

    Tempfix for ATVI bug

--- AssetHelper/AssetHelperPlugin.cs
+++ AssetHelper/AssetHelperPlugin.cs
@@ -1,5 +1,8 @@
+using AssetsTools.NET;
 using BepInEx;
 using BepInEx.Logging;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
 using Silksong.AssetHelper.BundleTools;
 using System.Collections;
 using System.Collections.Generic;
@@ -17,6 +20,8 @@ public partial class AssetHelperPlugin : BaseUnityPlugin
 
     private static readonly Dictionary<string, string> Keys = [];
 
+    private ILHook _atHook;
+
     private void Awake()
     {
         Instance = this;
@@ -25,7 +30,34 @@ public partial class AssetHelperPlugin : BaseUnityPlugin
         BundleDeps.Setup();
 
         GameEvents.Hook();
+
+        // TODO - remove this when assetstools.net gets updated
+        _atHook = new ILHook(typeof(AssetTypeValueIterator).GetMethod(nameof(AssetTypeValueIterator.ReadNext)), PatchATVI);
+        
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+    }
+
+    /// <summary>
+    /// Fixes a bug with AssetTypeValueIterator where it moves 4 bytes forward when reading a double rather than 8
+    /// </summary>
+    private void PatchATVI(ILContext il)
+    {
+        ILCursor cursor = new(il);
+
+        if (!cursor.TryGotoNext(MoveType.After,
+            i => i.MatchCallvirt<AssetTypeTemplateField>($"get_{nameof(AssetTypeTemplateField.ValueType)}"),
+            i => i.MatchStloc(out _),
+            i => i.MatchLdloc(out _),
+            i => i.MatchLdcI4(out _),
+            i => i.MatchSub(),
+            i => i.MatchSwitch(out _)
+            ))
+        {
+            return;
+        }
+
+        ILLabel[] switchArgs = (ILLabel[])cursor.Prev.Operand;
+        switchArgs[(int)AssetValueType.Double - 1] = switchArgs[(int)AssetValueType.Int64 - 1];
     }
 
     private IEnumerator Start()
